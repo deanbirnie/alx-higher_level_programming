dd5ea875f4d6896a627607465715dbb3bca10682
deanbirnie <deanicdl@gmail.com>
2023-05-09 11:10:35 -0700

    Task 6 - Fix Pycodestyle Errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i == 8 and j == 9:
-            print("{}{}".format(i,j))
+            print("{}{}".format(i, j))
         elif i != j and j > i:
             print("{}{}".format(i, j), end=", ")
-            
