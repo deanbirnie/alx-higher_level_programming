458164a6ed56e7764b8914807db4af71841c1716
deanbirnie <deanicdl@gmail.com>
2023-05-09 04:26:33 -0700

    Task 1 - Fix logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
 g5 = "and is greater than 5"
 l6n0 = "and is less than 6 and not 0"
 i0 = "and is 0"
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} {g5}")
-elif last_digit < 6 and not 0:
-    print(f"Last digit of {number} is {last_digit} {l6n0}")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} {i0}")
 else:
-    print("Last digit of {number} is {last_digit} {i0}")
+    print(f"Last digit of {number} is {last_digit} {l6n0}")
