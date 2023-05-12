fa386825ce492568b71f1476ab2ce5e3edc7cd9e
deanbirnie <deanicdl@gmail.com>
2023-05-12 06:02:39 -0700

    Task 2 - Fix logic argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
     args = len(argv) - 1
-    print("{} {}".format(args, "argument" if (args == 1) else "arguments"))
-    for i in range(1, (args + 1)):
-        print("{}: {}".format(i, argv[i]))
+    print("{} {}{}".format(args, "argument" if (args == 1) else "arguments",
+          "." if (args == 0) else ":"))
+    if args > 0:
+          for i in range(1, (args + 1)):
+              print("{}: {}".format(i, argv[i]))
