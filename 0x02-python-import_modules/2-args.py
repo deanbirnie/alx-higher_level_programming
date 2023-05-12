2fe67074de2731f8dd71a9369481208882b9eb9f
deanbirnie <deanicdl@gmail.com>
2023-05-12 04:55:17 -0700

    Task 2 - Fix import argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    args = len(sys.argv) - 1
+    from sys import argv
+    args = len(argv) - 1
     print("{} {}".format(args, "argument" if (args == 1) else "arguments"))
     for i in range(1, (args + 1)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, argv[i]))
