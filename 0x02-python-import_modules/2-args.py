835a3530f019d054c085d8b37d413c7d80e8228c
deanbirnie <deanicdl@gmail.com>
2023-05-12 06:14:05 -0700

    Task 2 - Pycodestyle fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     print("{} {}{}".format(args, "argument" if (args == 1) else "arguments",
           "." if (args == 0) else ":"))
     if args > 0:
-          for i in range(1, (args + 1)):
-              print("{}: {}".format(i, argv[i]))
+        for i in range(1, (args + 1)):
+            print("{}: {}".format(i, argv[i]))
