169b005864d23ded7dbcd21e490b9a7ab04ef81a
deanbirnie <deanicdl@gmail.com>
2023-05-29 13:56:13 -0700

    Task 7 - undo previous changes

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
     except ValueError as e:
         print("Exception: {}".format(e), file=sys.stderr)
