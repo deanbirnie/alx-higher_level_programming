45b479550190275f39a8520d768019555989c2ab
deanbirnie <deanicdl@gmail.com>
2023-05-29 06:43:09 -0700

    Task 2 - Safely print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_elements = 0
+    try:
+        for element in my_list:
+            try:
+                print("{:d}".format(int(element)), end='')
+                num_elements += 1
+                if num_elements == x:
+                    break
+            except (ValueError, TypeError):
+                continue
+    except TypeError:
+        pass
+
+    print("")
+
+    return num_elements
