95b26ca13346b5cbca130729c30dfc18b95a398a
deanbirnie <deanicdl@gmail.com>
2023-05-29 13:54:48 -0700

    Task 7 - Fix type casting error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
         return True
     except ValueError as e:
         print("Exception: {}".format(e), file=sys.stderr)
