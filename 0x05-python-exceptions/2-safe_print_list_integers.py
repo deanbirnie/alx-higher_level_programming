c7096f90f5b5f1ad5c3c27d42e14041f04b42549
deanbirnie <deanicdl@gmail.com>
2023-05-29 08:54:00 -0700

    Task 3 - Fix typecasting

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for element in my_list[:x]:
             try:
-                print("{:d}".format(int(element)), end='')
+                print("{:d}".format(element), end='')
                 num_elements += 1
                 if num_elements == x:
                     break
