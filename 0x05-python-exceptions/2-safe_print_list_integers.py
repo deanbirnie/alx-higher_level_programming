b174a6c6aa116c4f378cdf8d14ebc50a778dbeb9
deanbirnie <deanicdl@gmail.com>
2023-05-29 09:39:13 -0700

    Task 2 - Redo logic

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     num_elements = 0
-    try:
-        for element in my_list[:x]:
-            try:
-                print("{:d}".format(element), end='')
-                num_elements += 1
-                if num_elements == x:
-                    break
-            except (ValueError, TypeError):
-                continue
-    except TypeError:
-        pass
+    for element in my_list[:x]:
+        try:
+            print("{:d}".format(element), end='')
+            num_elements += 1
+        except (ValueError, TypeError):
+            pass
 
     print("")
 
