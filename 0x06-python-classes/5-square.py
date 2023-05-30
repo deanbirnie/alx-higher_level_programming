2a716a1126047897808c25e1e99a20a22554f51a
deanbirnie <deanicdl@gmail.com>
2023-05-30 14:06:56 -0700

    Task 5 - Print the square using # symbol

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """The square"""
+
+    def __init__(self, size=0):
+        """instantiate a new square
+
+        Args:
+        size (int): the size of the new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """size property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size property setter
+
+        Args:
+        value (int): the vlue for the size attribute
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates the area of the square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """use # to print out the square
+        """
+        for row in range(0, self.__size):
+            [print("#", end="") for col in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
