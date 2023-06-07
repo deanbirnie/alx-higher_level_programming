566d1f2cb94f9eaabab99d4d606b73be1c0b3427
deanbirnie <deanicdl@gmail.com>
2023-06-07 09:47:09 -0700

    Task 1 - Defining the rectangle in a class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""A module that defines a rectangle"""
+
+
+class Rectangle:
+    """Defining the characteristics of a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Defining attributes of the rectangle
+
+        Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width property setter
+
+        Args:
+        value (int): integer value of the width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height property setter
+
+        Args:
+        value (int): integer value of the height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
