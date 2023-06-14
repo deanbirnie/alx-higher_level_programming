8198e6ef61732fed36ae9872ef4817057e08b7a2
deanbirnie <deanicdl@gmail.com>
2023-06-14 07:32:29 -0700

    Task 1 - MyList inherits from list (object)

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""MyList inherits attributes and methods from list"""
+
+
+class MyList(list):
+    """inherits from MyList"""
+    def print_sorted(self):
+        """inherits from list"""
+        print(sorted(self))
