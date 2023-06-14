0273032c3f1e831d4d667a0bc828484cc19949d3
deanbirnie <deanicdl@gmail.com>
2023-06-14 07:41:47 -0700

    Task 2 - Object type and class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Check to see if an object is an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """Check type against specified class"""
+    return type(obj) is a_class
