43690bf108f793a63fda17cf57bb99ea3f818ef6
deanbirnie <deanicdl@gmail.com>
2023-06-14 16:36:21 -0700

    Task 0 - Fix documentation formatting

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Reading from a text file"""
 
+
 def read_file(filename=""):
     """Print the text from the specified file"""
     with open(filename, encoding="utf-8") as file:
