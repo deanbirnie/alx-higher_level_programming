ac623b53af1940f9bbfe5224d082c1715c799f87
deanbirnie <deanicdl@gmail.com>
2023-06-14 16:35:00 -0700

    Task 0 - Printing from UTF-8 text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reading from a text file"""
+
+def read_file(filename=""):
+    """Print the text from the specified file"""
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
