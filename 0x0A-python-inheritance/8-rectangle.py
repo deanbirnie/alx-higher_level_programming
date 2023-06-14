1a1fd3769548a4bbe3aa96f48ad97a6ee8fc7067
deanbirnie <deanicdl@gmail.com>
2023-06-14 15:14:34 -0700

    Task 8 - Fix the way the base class is referenced

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""Raises an exception in the method area()"""
+"""Import 7-base_geometry for base case"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class BaseGeometry:
-    """base class"""
-
-    def area(self):
-        """area raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """input integer validation"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Defining a rectangle"""
