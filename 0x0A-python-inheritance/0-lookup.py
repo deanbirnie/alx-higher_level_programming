84fc8d5b2fa7097feb6d1555b031572d1131a7b0
deanbirnie <deanicdl@gmail.com>
2023-06-14 07:18:03 -0700

    Task 0 - Fix documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Returns a list of all available attributes and methods of an object"""
+
+
 def lookup(obj):
+    """Looks for all attributes and methods of an object"""
     return dir(obj)
