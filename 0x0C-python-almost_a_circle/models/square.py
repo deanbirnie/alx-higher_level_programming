2919fa5ffb8aac97c26173348e3b0e5d8ab4c9ba
deanbirnie <deanicdl@gmail.com>
2023-06-23 16:32:00 -0700

    Task 10 - Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defining a square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defining the square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiation"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """string representation"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
