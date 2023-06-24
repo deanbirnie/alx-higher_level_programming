7aa61d95c38cac838f3510a7733cb087eaafd463
deanbirnie <deanicdl@gmail.com>
2023-06-23 17:20:40 -0700

    Task 16 - Writing JSON string representation to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,14 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        json_string = cls.to_json_string([obj.to_dictionary()
+                                          for obj in list_objs])
+        with open(filename, 'w') as file:
+            file.write(json_string)
