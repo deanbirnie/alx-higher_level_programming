b419391fc0f4d9b2a6614abf0d9af46bbe514284
deanbirnie <deanicdl@gmail.com>
2023-06-23 17:17:18 -0700

    Task 15 - Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """JSON representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
