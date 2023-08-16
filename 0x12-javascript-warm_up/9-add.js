6fff7671a02e72b90b6cc31e04dc4ecf5df6e15a
deanbirnie <deanicdl@gmail.com>
2023-08-16 13:57:01 -0700

    Task 9 - adding two integers passed as arguments

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const [, , arg1, arg2] = process.argv;
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+
+if (!isNaN(num1) && !isNaN(num2)) {
+  const result = add(num1, num2);
+  console.log(result);
+} else {
+    console.log('NaN');
+}
