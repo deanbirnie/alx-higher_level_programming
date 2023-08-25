bcebc04e0eee5034a91848bfd42034e059f36aa1
deanbirnie <deanicdl@gmail.com>
2023-08-25 13:22:28 -0700

    Task 5 - fix pycodestyle, line length

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,9 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>".format(
+            sys.argv[0])
+        )
         sys.exit(1)
 
     username = sys.argv[1]
