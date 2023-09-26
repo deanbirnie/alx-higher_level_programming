49d0c5a3758d9f81e8dfedc12c1ee857271feb24
deanbirnie <deanicdl@gmail.com>
2023-09-26 06:39:37 -0700

    Task 3 - Print title of Star Wars movie where the episode number matches a given integer

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+
+  try {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } catch (e) {
+    console.error(e);
+  }
+});
